6ead30a649909b7b7c3a5f9512910cbae2e2a442
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-07-22 15:35:24 +0200

    Update App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '../button/Button';
+// import { Button } from 'reactstrap';
 import logo from '../../assets/images/logo.svg';
 import './App.css';
 
@@ -18,6 +20,8 @@ function App() {
         >
           Learn React
         </a>
+        <Button color="danger">Danger!</Button>
+        {/*<button onClick={this.buttonClicked}>Meet Someone New</button>*/}
       </header>
     </div>
   );
